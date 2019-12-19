3635764d874cceb524c44106d05df318f6a3d5ac
Zarakick <kmarzola21@gmail.com>
2019-12-19 09:21:35 -0500

    Cambios finales para almacenar informacion en logs

--- src/main/java/com/godxvincent/kafkaservices/utilities/Constants.java
+++ src/main/java/com/godxvincent/kafkaservices/utilities/Constants.java
@@ -2,7 +2,7 @@ package com.godxvincent.kafkaservices.utilities;
 
 public class Constants {
 
-    public static  final String TOPICS = "demo";
-    public static final String  GROUP_ID = "demo";
+    public static  final String TOPICS = "test";
+    public static final String  GROUP_ID = "test";
 
 }
